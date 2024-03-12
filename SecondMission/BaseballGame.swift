ad9549aa7bed60f6f8c17f7cd73cefc819209ba0
SijongKim93 <rlatlwhd456@gmail.com>
2024-03-12 17:48:10 +0900

    Lv 1 시작(베이스볼 게임 코드 구성 진행)

---
+++ SecondMission/BaseballGame.swift
@@ -0,0 +1,10 @@
+//
+//  BaseballGame.swift
+//  SecondMission
+//
+//  Created by 김시종 on 3/12/24.
+//
+
+import Foundation
+
+
---
+++ SecondMission/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  SecondMission
+//
+//  Created by 김시종 on 3/12/24.
+//
+
+import Foundation
+
+class BaseballGame {
+    func start() {
+        print("숫자 야구 게임을 시작합니다.")
+    }
+}
+
+let game = BaseballGame()
+game.start()
+
