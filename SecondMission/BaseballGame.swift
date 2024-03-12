3bbeda8ec327ab25df1889ea45c7d1ac44920f5a
SijongKim93 <rlatlwhd456@gmail.com>
2024-03-12 21:47:16 +0900

    Lv 1 구현 완료 ( 플레이어가 선택한 숫자 3개와 컴퓨터가 랜덤으로 설정한 숫자 3개 출력 완료 ( 숫자 비교 후 스트라이크 , 볼 판정 판단 유무 구현 예정)

--- SecondMission/BaseballGame.swift
+++ SecondMission/BaseballGame.swift
@@ -7,4 +7,51 @@
 
 import Foundation
 
+class RandamNumber {
+    
+    func playerSelect(_ number1: Int, _ number2: Int, _ number3: Int) -> [Int] {
+        let select = [number1, number2, number3]
+        return select
+    }
 
+    
+    //플레이어가 값을 입력
+    func select() {
+        guard let input = readLine() else {
+            print("입력이 없습니다.")
+            return
+        }
+        let numbers: [Int] = input.compactMap { Int(String($0)) }
+        
+        if numbers.count != 3 {
+            print("세 개의 숫자를 입력해주세요.")
+            return
+        }
+        
+        let number1 = numbers[0]
+        let number2 = numbers[1]
+        let number3 = numbers[2]
+        
+    }
+    
+    //컴퓨터가 랜덤으로 값을 저장
+    func start() {
+        var comChoice: [Int]
+        repeat {
+            let comNumber1: Int = Int.random(in: 0...9)
+            let comNumber2: Int = Int.random(in: 0...9)
+            let comNumber3: Int = Int.random(in: 0...9)
+            
+            comChoice = [comNumber1, comNumber2, comNumber3]
+        } while Set(comChoice).count < 3
+        
+        let comNumber1 = comChoice[0]
+        let comNumber2 = comChoice[1]
+        let comNumber3 = comChoice[2]
+        
+        //중복 값 제거
+        print("컴퓨터가 선택한 값\(comChoice)")
+    }
+    
+    
+}
--- SecondMission/main.swift
+++ SecondMission/main.swift
@@ -10,9 +10,14 @@ import Foundation
 class BaseballGame {
     func start() {
         print("숫자 야구 게임을 시작합니다.")
+        let randomNumberGame = RandamNumber()
+        randomNumberGame.select()
+        randomNumberGame.start()
+        
     }
 }
 
 let game = BaseballGame()
 game.start()
+
 
